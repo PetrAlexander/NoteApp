40fd9bf9e8802ae6754a4785b6bf6aaceb195413
Alexander <poltavetz.alexander@mail.ru>
2023-02-22 12:58:50 +0300

    Added skeleton layer data

--- app/src/main/java/com/example/noteapp/domain/NoteItem.kt
+++ app/src/main/java/com/example/noteapp/domain/NoteItem.kt
@@ -1,8 +1,12 @@
 package com.example.noteapp.domain
 
 data class NoteItem(
-    val id: Int,
     val name: String,
     val priority: Int,
-    val enabled: Boolean
-)
+    val enabled: Boolean,
+    var id: Int = UNDEFINED_ID
+) {
+    companion object {
+        const val UNDEFINED_ID = -1
+    }
+}
